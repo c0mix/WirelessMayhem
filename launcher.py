7b45b13b7752b39571f8757400b585077487be85
c0mix <lorenzo.comi@studenti.unimi.it>
2017-02-12 14:10:39 +0100

    ok interface Mets & refactoring

--- launcher.py
+++ launcher.py
@@ -15,9 +15,11 @@ ast = '['+B+'*'+W+'] '
 min = '['+R+'-'+W+'] '
 plu = '['+G+'+'+W+'] '
 
+
 def check_root():
     if not os.geteuid() == 0:
         sys.exit(R+'WirelessPrism must be run as root... Exiting'+W)
+
 
 def print_menu():
     print('\n'+G + 'Welcome in Wireless Mayhem Framework, please choose one of the following activities:' + W)
@@ -27,76 +29,56 @@ def print_menu():
     print(O + '[4]' + W + ' Mail Sniffer')
     print(O + '[5]' + W + ' Airodump')
 
+
 def selectMON_interface():
     print B+'[INFO]'+W+' Looking for a monitor-mode interface'
-
-    # cerca interfacce in monitor mode
     cmd = "ifconfig -a | grep mon"
-    try:
+    try: # cerca interfacce in monitor mode
         mon_interfaces = subprocess.check_output(cmd, shell=True)
         print mon_interfaces
 
     except:
-        print R+'[INFO]'+W+' No monitor-mode interfaces found'
+        print R+'[ERROR]'+W+' No monitor-mode interfaces found'
         mon_interfaces = False
 
-    # cerca interfacce wireless
+    # cerca interfacce wireless wlanX
     if not mon_interfaces:
         cmd = "ifconfig -a | grep wlan"
         print B+'[INFO]'+W+' Looking for a Wlan interface'
 
         try:
             wlan_interfaces = subprocess.check_output(cmd, shell=True)
-
+            print G + '[INFO]' + W + ' Wlan found'
         except:
             # non ci sono nemmeno interfacce wireless
-            print R+'[INFO]'+W+' No Wlan interfaces found... Exiting'
+            print R+'[ERROR]'+W+' No Wlan interfaces found... Exiting'
             return False
 
         # ci sono interfacce ma esci e metti in monitor
         if wlan_interfaces:
-            print G+'[INFO]'+W+' Wlan found'
             print B+'[INFO]'+W+' Please put in monitor mode ("$ airmon-ng start wlanX") one of these interfaces:\n'+wlan_interfaces
             return False
     else:
         interface = raw_input('Enter a Monitor interface: ')
-
     return interface
 
 
 def selectWLAN_interface():
-    '''
-    print B+'[INFO]'+W+' Looking for a monitor-mode interface'
-
-    # cerca interfacce in monitor mode
-    cmd = "ifconfig -a | grep mon"
-    try:
-        mon_interfaces = subprocess.check_output(cmd, shell=True)
-        print mon_interfaces
-
-    except:
-        print R+'[INFO]'+W+' No monitor-mode interfaces found'
-        mon_interfaces = False
-
-    # cerca interfacce wireless
-    if not mon_interfaces:
-    '''
-
     print B+'[INFO]'+W+' Looking for a Wlan interface'
     cmd = "ifconfig -a | grep wlan"
 
-    try:
+    try: #cerca wlan
         wlan_interfaces = subprocess.check_output(cmd, shell=True)
+        print G + '[INFO]' + W + ' Wlan found'
+        print wlan_interfaces
 
     except:
-        # non ci sono nemmeno interfacce wireless
-        print R+'[INFO]'+W+' No Wlan interfaces found... Exiting'
+        # non ci sono interfacce wireless
+        print R+'[ERROR]'+W+' No Wlan interfaces found... Exiting'
         return False
 
-    # ci sono interfacce ma esci e metti in monitor
-    print G+'[INFO]'+W+' Wlan found'
-    print B+'[INFO]'+W+' Please put in monitor mode ("$ airmon-ng start wlanX") one of these interfaces:\n'+wlan_interfaces
-    interface = raw_input('Enter a Monitor interface: ')
+    # ci sono interfacce!
+    interface = raw_input('Enter a Wlan interface: ')
     return interface
 
 
@@ -104,79 +86,82 @@ def select_filename():
     filename = raw_input('Enter a Filename: ')
     return filename
 
+
 def main():
     check_root()
     while True:
+        #loop forever looking for an option
         try:
             print_menu()
             input = raw_input()
 
+            # options list
             if input == '1':
-                print G+'[INFO] Starting Wi-Fi SSID Sniffer'+ W
                 interface = selectMON_interface()
                 if interface:
+                    print G + '[INFO] Starting Wi-Fi SSID Sniffer' + W
                     try:
                         sniff(iface=interface, prn=Sniffer.ssidSniffer)
                     except(KeyboardInterrupt, SystemExit):
                         pass
                 else:
-                    print R+'[INFO] Exiting'+ W
+                    print R +'[INFO]'+ W+' Exiting'
                     exit(1)
 
             elif input == '2':
-                print G+'[INFO] Starting Sensible Data Sniffer'+ W
                 interface = selectMON_interface()
                 if interface:
+                    print G + '[INFO] Starting Sensible Data Sniffer' + W
                     try:
                         sniff(filter='tcp', iface=interface, prn=Sniffer.sendibleDataSniff, store=0)
                     except(KeyboardInterrupt, SystemExit):
                         pass
                 else:
-                    print R + '[INFO] Exiting' + W
+                    print R +'[INFO]'+ W+' Exiting'
                     exit(1)
 
             elif input == '3':
-                print G+'[INFO] Starting FTP Credential Sniffer'+ W
                 interface = selectWLAN_interface()
                 if interface:
+                    print G + '[INFO] Starting FTP Credential Sniffer' + W
                     try:
                         sniff(filter='tcp port 21', iface=interface, prn=Sniffer.ftpSniff, store=0)
                     except(KeyboardInterrupt, SystemExit):
                         pass
                 else:
-                    print R + '[INFO] Exiting' + W
+                    print R +'[INFO]'+ W+' Exiting'
                     exit(1)
 
             elif input == '4':
-                print G+'[INFO] Starting Mail Sniffer'+ W
                 interface = selectWLAN_interface()
                 if interface:
+                    print G + '[INFO] Starting Mail Sniffer' + W
                     try:
                         sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=Sniffer.mailSniff, store=0)
                     except(KeyboardInterrupt, SystemExit):
                         pass
                 else:
-                    print R + '[INFO] Exiting' + W
+                    print R +'[INFO]'+ W+' Exiting'
                     exit(1)
 
             elif input == '5':
-                print G+'[INFO] Starting AirDump'+ W
                 interface = selectMON_interface()
                 if interface:
+                    print G + '[INFO] Starting AirDump' + W
                     filename = select_filename()
                     try:
                         Sniffer.Airdump(interface, filename)
                     except(KeyboardInterrupt, SystemExit):
                         pass
                 else:
-                    print R + '[INFO] Exiting' + W
+                    print R +'[INFO]'+ W+' Exiting'
                     exit(1)
 
                 time.sleep(1)
 
-
+        #exiting with ctrlC
         except KeyboardInterrupt:
-            print R + '[INFO] Exiting' + W
+            print R +'[INFO]'+ W+' Exiting'
             exit(0)
 
 if __name__ == '__main__':
